551885b181654e95d50ff10f4730685bd2854783
Kamil Foltynski <kamil@foltynski.com>
2024-10-31 15:33:18 +0100

    applied styling

--- apps/cohortCharacterization/helpers.R
+++ apps/cohortCharacterization/helpers.R
@@ -9,13 +9,15 @@ split_camel_case <- function(camelCaseString) {
 }
 
 make_datasource_summary_html <- function(datasource_summary) {
-  if (length(datasource_summary) == 0)
+  if (length(datasource_summary) == 0) {
     return(NULL)
-  datasource_summary %>% 
+  }
+  datasource_summary %>%
     names() %>%
     lapply(function(nm) {
       tags$li(strong(tolower(split_camel_case(nm))), ": ", datasource_summary[[nm]])
-    }) %>% tags$ol()
+    }) %>%
+    tags$ol()
 }
 
 make_intro_page <- function(PROPERTIES,
@@ -25,26 +27,28 @@ make_intro_page <- function(PROPERTIES,
                             add_referenced_cohorts = FALSE,
                             add_cohort_summary = FALSE) {
   stopifnot("PROPERTIES arg is missing" = !missing(PROPERTIES))
-  app_type <- match.arg(app_type, c("cohort_counts", "cohort_characterization", "incidence_rate", 
-                                    "cohort_pathway"))
+  app_type <- match.arg(app_type, c(
+    "cohort_counts", "cohort_characterization", "incidence_rate",
+    "cohort_pathway"
+  ))
   if (add_cohort_summary) {
     stopifnot("markdown_file arg is missing" = !missing(markdown_file))
-    if (!file.exists(markdown_file))
+    if (!file.exists(markdown_file)) {
       stop(glue("'{markdown_file}' does not exist"))
+    }
   }
-  
+
   stopifnot("datasource_summary_file arg is missing" = !missing(datasource_summary_file))
   datasource_summary <- jsonlite::read_json(datasource_summary_file)
-  datasource_summary_html <- make_datasource_summary_html(datasource_summary) %||% 
+  datasource_summary_html <- make_datasource_summary_html(datasource_summary) %||%
     shiny::tags$span(style = "color: grey;", "Data source summary is empty.")
-  
+
   not_found <- shiny::tags$span(style = "color: orange;", "Property not found.")
-  
+
   shiny::tagList(
     h1("Study Asset Description:"),
-    
     tags$ol(
-      tags$li(strong("Name: "), PROPERTIES$cohort_name  %||% PROPERTIES$analysis_name %||% PROPERTIES$asset_name %||% not_found),
+      tags$li(strong("Name: "), PROPERTIES$cohort_name %||% PROPERTIES$analysis_name %||% PROPERTIES$asset_name %||% not_found),
       tags$li(strong("ID: "), PROPERTIES$asset_id %||% not_found),
       tags$li(strong("Author: "), PROPERTIES$author %||% not_found),
       tags$li(strong("Data Source: "), PROPERTIES$datasource_name %||% not_found),
@@ -70,44 +74,37 @@ make_intro_page <- function(PROPERTIES,
       if (app_type == "app_type") tags$li(strong("Records: "), PROPERTIES$record_count %||% not_found),
       if (app_type == "app_type") tags$li(strong("People: "), PROPERTIES$person_count %||% not_found)
     ),
-    
     if (add_cohort_summary) {
       tagList(
         h1("Cohort Summary:"),
         shiny::includeMarkdown(path = markdown_file),
       )
     },
-    
     h1("Dashboard Functionality:"),
-    
     dashboard_func(app_type = app_type),
-    
     h1("Author Description:"),
     div(PROPERTIES$author_notes %||% not_found),
-    
-    
     h1("Data Source Summary:"),
     datasource_summary_html,
-    
+
     # p("The description of the cohort being analysed is: ", textOutput("description"))
   )
 }
 
 
 dashboard_func <- function(app_type) {
-  app_type <- match.arg(app_type, c("cohort_counts", "cohort_characterization", "incidence_rate", 
-                                    "cohort_pathway"))
+  app_type <- match.arg(app_type, c(
+    "cohort_counts", "cohort_characterization", "incidence_rate",
+    "cohort_pathway"
+  ))
   type <- list(
     "cohort_counts" = tagList(
       div("The application provides important statistics and charts to describe cohort counts and percentages of persons or records in the cohort after applying a certain set of inclusion rules."),
-      
-      
       span(strong("Cohort unit: "), "Briefly describes how this function works within the app."),
       tags$ol(
         tags$li("Person: the person cohort unit is an individual fitting the criteria for inclusion in a cohort."),
         tags$li("Event: an event unit is an observable event from the data and is a building block of healthcare data that is tied to a person unit. ")
       ),
-      
       span(strong("View type: "), "Toggle between person and event units by clicking the buttons on the top left."),
       tags$ol(
         tags$li("Intersect: describes a means of defining a cohort based on the intersection of multiple events (e.g. If you want to define a cohort of persons diagnosed with hypertension that took an ACE inhibitor within the last 30 days the intersect view would capture only those that fit both criteria in the applied time frame)."),
@@ -118,7 +115,7 @@ dashboard_func <- function(app_type) {
     "incidence_rate" = span("Once you have a built analysis for incidence in the Atlas platform the RShiny app offers a visual presentation of your analysis."),
     "cohort_pathway" = span("The RShiny app will display cohort pathway results with an interactive sunburst. Users can click on the sunburst to see details for the segment on the top right. Cohort details are available on the top left. Scroll down past the image to see the tabular view if preferred.")
   )
-  
+
   type[[app_type]]
 }
 
